1ee8d1ef90cdf09ddd156b4596064d2decb7315c
Yogesh Kumar <121163370+Yogesh4732@users.noreply.github.com>
2026-01-06 12:12:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
-const express = require('express')
-const articleRouter = require("./routes/articles")
-const Article = require('./models/article')
-const mongoose = require('mongoose')
-const methodOverride = require('method-override')
-const app = express()
+const express = require('express');
+const articleRouter = require('./routes/articles');
+const Article = require('./models/article');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
-mongoose.connect('mongodb://localhost/myBlogAPI')
-app.set("views", "./view")
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
-app.use(methodOverride('_method'))
-app.get('/', async(req, res) => {
-    const articles =await Article.find().sort({ createdAt:'desc'})
-    res.render('articles/index', { articles: articles })
-})
+const app = express();
 
-app.use('/articles', articleRouter)
+/* ---------------- DATABASE ---------------- */
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
-app.listen(3000)
+/* ---------------- VIEW ENGINE ---------------- */
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+/* ---------------- MIDDLEWARE ---------------- */
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+/* ---------------- ROUTES ---------------- */
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' });
+  res.render('articles/index', { articles });
+});
+
+app.use('/articles', articleRouter);
+
+/* ---------------- SERVER ---------------- */
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
